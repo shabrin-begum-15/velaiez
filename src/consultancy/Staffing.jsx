7693719f580d853daaa5862d07c20b8082de3fca
shabrin-begum-15 <shabrinbegum1510@gmail.com>
2025-06-02 11:31:40 +0530

    Update Staffing.jsx

--- src/consultancy/Staffing.jsx
+++ src/consultancy/Staffing.jsx
@@ -20,12 +20,12 @@ const Staffing = () => {
           <p className="service-label">OUR SERVICES</p>
           <h2>Temporary Staffing</h2>
           <div className="temp-description">
-            <h3>Flexible, cost-effective workforce solutions:</h3>
+            <h3>Short-term demands require agile solutions. Our temporary staffing services ensure you get the right talent at the right time to support your ongoing projects.</h3>
             <ul>
-              <li><strong>Contract Staffing:</strong> Short-term roles based on project demands</li>
-              <li><strong>Payroll Management:</strong> Compliance, taxes, and salary processing</li>
-              <li><strong>Workforce Flexibility:</strong> Scale teams up or down efficiently</li>
-              <li><strong>Quick Deployment:</strong> Fast turnaround for immediate needs</li>
+              <li><strong>Contract Staffing:</strong> Optimize your workforce with contract staffing. Our Consultant connect you with talented professionals who bring expertise and agility to temporary or specialized roles.</li>
+              <li><strong>Payroll Management:</strong> Streamline your payroll operations with our expert solutions. From salary calculations to statutory deductions, we ensure precision and reliability.</li>
+              <li><strong>Workforce Flexibility:</strong>Optimize productivity with dynamic workforce strategies. We ensure you have the right talent in place, exactly when you need them.</li>
+              <li><strong>Quick Deployment:</strong> Speed up hiring with our quick deployment services. We connect you with skilled professionals ready to contribute from day one.</li>
             </ul>
           </div>
         </div>
